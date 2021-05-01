d27bb3b7b15f23d9e2d08f16e630f6ae15efcd78
user <oreomasida@gmail.com>
2021-05-01 15:36:58 +0900

    feature: series D 3rd class

--- week03/scraping.py
+++ week03/scraping.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -7,4 +8,19 @@ response = requests.get(
     headers = headers
 )
 
-print(response.text)
+# print(response.text)
+
+soup = BeautifulSoup(
+    response.text, 'html.parser'
+)
+
+selector = '#old_content > table > tbody > tr'
+title_selector = 'td.title > div > a'
+
+titles = soup.select(selector)
+for title in titles:
+    title_tag = title.select_one(title_selector)
+    if title_tag:
+        print(title_tag.text)
+
+
