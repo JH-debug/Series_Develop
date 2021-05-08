cefafc1efb17d3a9c66060115ddd687c7dd3bffe
user <oreomasida@gmail.com>
2021-05-08 14:37:54 +0900

    크롤링해서 정보 저장

--- alonemomo/app.py
+++ alonemomo/app.py
@@ -1,12 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+# mongodb 추가
+client = MongoClient('localhost', 27017)
+db = client.get_database('sparta')
 
 # API 추가
 @app.route('/', methods = ['GET'])  # 데코레이터 문법
 def index():  # 함수 이름은 고유해야 함
     return render_template('index.html', test = '테스트')
+
+
+# 아티클 추가 API
+@app.route('/memo', methods = ['POST'])
+def save_memo():
+    form = request.form
+    url_receive = form['url_give']
+    comment_receive = form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    response = requests.get(
+        url_receive,
+        headers=headers
+    )
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')
+    url = soup.select_one('meta[property="og:url"]')
+    image = soup.select_one('meta[property="og:image"]')
+    description = soup.select_one('meta[property="og:description"]')
+    print(title['content'])
+    print(url['content'])
+    print(image['content'])
+    print(description['content'])
+
+    document = {
+        'title': title['content'],
+        'url': url['content'],
+        'image': image['content'],
+        'description': description['content'],
+        'comment': comment_receive
+    }
+
+    db.articles.insert_one(document)
+    return jsonify(
+        {'result': 'success'}
+    )
+
 
 # app.py 파일 실행
 if __name__ == '__main__':
