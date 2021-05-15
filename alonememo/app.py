57a5263edc519401557e97754d8b15ef329123d8
user <oreomasida@gmail.com>
2021-05-15 11:56:28 +0900

    로그인 jwt

--- alonememo/app.py
+++ alonememo/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
+import datetime
 import hashlib
+import os
+import jwt as jwt
+from flask import Flask, render_template, request, jsonify
+from flask.cli import load_dotenv
+from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,6 +14,12 @@ app = Flask(__name__)
 # mongodb 추가
 client = MongoClient('localhost', 27017)
 db = client.get_database('sparta')
+
+# .env 파일로 환경변수로 설정
+load_dotenv()
+# 환경변수 읽어오기
+JWT_SECRET = os.environ['JWT_SECRET']
+
 
 # API 추가
 @app.route('/', methods = ['GET'])  # 데코레이터 문법
@@ -33,7 +43,30 @@ def api_login():
     id = request.form['id_give']
     pw = request.form['pw_give']
 
-    # TODO id, pw 검증 후에 JWT 만들어서 RETURN
+    # id, pw 검증 후에 JWT 만들어서 return
+    pw_hash = hashlib.sha256(pw.encode()).hexdigest()
+
+    user = db.users.find_one({'id': id, 'pw': pw_hash}, {'_id': False})
+
+    # 가입 상태
+    if user:
+        # jwt가 1시간 동안 유효
+        expiration_time = datetime.timedelta(hours=1)
+        # 현재 시간 + 1
+        payload = {
+            'id': id,
+            'exp': datetime.datetime.utcnow() + expiration_time
+        }
+        # 코드로 관리해서는 안되는 비밀값은 아래와 같이 처리
+        token = jwt.encode(payload, JWT_SECRET)
+        print(token)
+
+        return jsonify({'result': 'success', 'token': token})
+
+
+    # 가입하지 않은 상태
+    else:
+        return jsonify({'result': 'fail', 'msg': '로그인 실패'})
 
 
 @app.route('/api/register', methods=['POST'])
