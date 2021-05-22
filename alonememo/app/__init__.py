e6a5e0c75a48ee811e3387fc748798ead0361d95
user <oreomasida@gmail.com>
2021-05-22 10:23:35 +0900

    app package 구조로 수정

--- alonememo/app/__init__.py
+++ alonememo/app/__init__.py
@@ -1,27 +1,94 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
 
+
+app = Flask(__name__)
+
+# mongodb 추가
 client = MongoClient('localhost', 27017)
-db = None
+db = client.get_database('sparta')
+
+# API 추가
+@app.route('/', methods = ['GET'])  # 데코레이터 문법
+def index():  # 함수 이름은 고유해야 함
+    memos = list(db.articles.find({}, {'_id': False}))
+    return render_template('index.html', test = '테스트', memos=memos)
+
+# 로그인 화면
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+# 가입 화면
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    id = requests.form['id_give']
+    pw = requests.form['pw_give']
+
+    # TODO id, pw 검증 후에 JWT 만들어서 RETURN
 
 
-# 플라스크 프레임워크에서 지정한 함수 이름
-# 플라스크 동작할 때, create_app() 함수 결과로 리턴한 앱 실행
-def create_app(database_name='sparta'):
-    # 플라스크 웹 서버 생성하기
-    app = Flask(__name__)
-    app.debug = True
-    app.config.from_pyfile('config.py')
 
-    global db
-    db = client.get_database(database_name)
 
-    # 순환 참조 방지
-    from app.views import api, main, memo, user
+# 아티클 추가 API
+@app.route('/memo', methods = ['POST'])
+def save_memo():
+    form = request.form
+    url_receive = form['url_give']
+    comment_receive = form['comment_give']
 
-    app.register_blueprint(api.bp)
-    app.register_blueprint(main.bp)
-    app.register_blueprint(memo.bp)
-    app.register_blueprint(user.bp)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    response = requests.get(
+        url_receive,
+        headers=headers
+    )
 
-    return app
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')
+    url = soup.select_one('meta[property="og:url"]')
+    image = soup.select_one('meta[property="og:image"]')
+    description = soup.select_one('meta[property="og:description"]')
+    print(title['content'])
+    print(url['content'])
+    print(image['content'])
+    print(description['content'])
+
+    document = {
+        'title': title['content'],
+        'url': url['content'],
+        'image': image['content'],
+        'description': description['content'],
+        'comment': comment_receive
+    }
+
+    db.articles.insert_one(document)
+    return jsonify(
+        {'result': 'success', 'msg': '저장됐습니다.'}
+    )
+
+# 아티클 로드 API
+@app.route('/memo', methods=['GET'])
+def list_memo():
+    memos = list(db.articles.find({}, {'_id': False}))
+    return jsonify({
+        'result': 'success',
+        'articles': memos,
+        'msg': '로드합니다.'}
+    )
+
+# app.py 파일 실행
+if __name__ == '__main__':
+    app.run(
+        '0.0.0.0',    # 모든 IP에서 오는 요청 허용
+        7000,         # 플라스크 웹 서버는 7000번 포트 사용
+        debug = True  # 에러 발생 시 에러 로그 보여줌
+    )
+
